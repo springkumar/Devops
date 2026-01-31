cef07787395f42be877530c3b6133a52824f7aee
springkumar <32031795+springkumar@users.noreply.github.com>
2026-01-31 13:18:17 +0530

    Second Commit operation

--- Test.py
+++ Test.py
@@ -1,9 +1,7 @@
-with open("/var/lib/jenkins/workspace/Auto_Test_Project/output.txt", "r") as f:
-    content = f.read().strip()   # "Addition: 30"
+with open("output.txt", "r") as f:
+    content = f.read().strip()
 
-# Extract number
-result = int(content.split(":")[1].strip())
+result = int(content)
 
-# Test condition
 assert result == 30
 print("Test passed successfully")
